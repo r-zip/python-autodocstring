37dc2853f392e61e6c5f08cf0183c0549df91546
Ryan Pilgrim <ryan.pilgrim@infiniaml.com>
2019-12-01 21:40:12 -0500

    rewrite recursively

--- autodocstring/autodocstring.py
+++ autodocstring/autodocstring.py
@@ -1,89 +1,46 @@
-from itertools import dropwhile, takewhile
-from uuid import uuid4
-from typing import Any, Dict, List
+from typing import Dict, List
 
 import parso
-from parso.python.tree import Function
+from parso.python.tree import ClassOrFunc, Module
 
 
-class Interval:
-    def __init__(self, low: float, high: float):
-        self.low = low
-        self.high = high
+class Foo:
+    def __init__(self):
+        pass
 
-    def contains(self, other: "Interval") -> bool:
-        if other.low <= self.low and self.high <= other.high:
-            return True
-        return False
-
-    def overlaps_with(self, other: "Interval") -> bool:
-        if self.high <= other.low or self.low >= other.high:
-            return True
-        return False
+    def bar(self):
+        pass
 
 
-def get_funcdefs_at_point(uri: str, current_line: int, innermost: bool = True) -> List[Function]:
+def get_parse_tree(uri: str) -> Module:
     with open(uri) as f:
         code = f.read()
-    tree = parso.parse(code)
-    funcdefs = list(tree.iter_funcdefs())
-    funcdefs_at_point = []
 
-    for funcdef in funcdefs:
-        if funcdef.start_pos[0] <= current_line <= funcdef.end_pos[0]:
-            funcdefs_at_point.append(funcdef)
-
-    return funcdefs_at_point
+    return parso.parse(code)
 
 
-def to_code(node: Any) -> str:
-    if node is None:
-        return None
-    return node.get_code().strip()
+def get_toplevel_defs(uri: str) -> Dict:
+    tree = get_parse_tree(uri)
+    toplevel_funcdefs = list(tree.iter_funcdefs())
+    toplevel_classdefs = list(tree.iter_classdefs())
+    return [*toplevel_funcdefs, *toplevel_classdefs]
 
 
-def is_arrow(node: Any) -> str:
-    return node.type == "operator" and node.value == "->"
-
-
-def is_colon(node: Any) -> str:
-    return node.type == "operator" and node.value == ":"
-
-
-# TODO: get outermost or innermost docstring
-def docstring_info(uri: str, line: int) -> Dict:
-    # if there are no function definitions at the current line, return None
-    funcdef = get_funcdefs_at_point(uri, line)[0]
-    if funcdef is None:
+def get_enclosing_def(uri: str, line: int, defs: List) -> ClassOrFunc:
+    # base cases
+    if len(defs) == 0:
         return None
 
-    # get the function name, parameters, and return typehint
-    func_name = funcdef.name.value
-    # TODO: don't let nested definitions pollute these lists
-    parameters = [node for node in funcdef.children if node.type == "parameters"][0]
-    params = [p for p in parameters.children if p.type == "param"]
+    for def_ in defs:
+        if isinstance(def_, ClassOrFunc) and def_.start_pos[0] <= line and def_.end_pos[0] >= line:
+            nested_defs = list(def_.iter_funcdefs()) + list(def_.iter_classdefs())
+            return get_enclosing_def(uri, line, nested_defs) or def_
 
-    return_typehint = None
-    # if there's a return typehint, get it
-    if any(is_arrow(n) for n in funcdef.children) and any(is_arrow(n) for n in funcdef.children):
-        # take only the stuff between the arrow and the colon
-        children = dropwhile(lambda x: not is_arrow(x), funcdef.children)
-        children = list(takewhile(lambda x: not is_colon(x), children))
-        if len(children) == 2:
-            return_typehint = to_code(list(children)[1])
-
-    # return the response as a dictionary
-    response = {
-        "func_name": func_name,
-        "params": [
-            {"name": p.name.value, "annotation": to_code(p.annotation), "default": to_code(p.default)} for p in params
-        ],
-        "return_typehint": return_typehint,
-        "id": str(uuid4()),
-    }
-
-    return response
+    return None
 
 
 if __name__ == "__main__":
-    docstring_info("autodocstring/autodocstring.py", 67)
+    uri = "autodocstring/autodocstring.py"
+    line = 39
+    toplevel_defs = get_toplevel_defs(uri)
+    enclosing_funcdef = get_enclosing_def(uri, line, toplevel_defs)
