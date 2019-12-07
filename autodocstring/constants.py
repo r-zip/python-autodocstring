266cadd40d25b05e06f33c7ffa1335eedb118a2d
Ryan Pilgrim <ryan.pilgrim@infiniaml.com>
2019-12-07 15:32:38 -0500

    add first pass at jinja template

--- autodocstring/constants.py
+++ autodocstring/constants.py
@@ -1,2 +1,5 @@
+from jinja2 import Environment, PackageLoader
+
 DEFAULT_HOST: str = "localhost"
 DEFAULT_PORT: int = 5000
+TEMPLATE_ENV = Environment(loader=PackageLoader("autodocstring", "templates"))
