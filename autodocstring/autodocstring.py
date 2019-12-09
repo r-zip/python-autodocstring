869fa3ea8ef9f99bca6bf8c37d8b7646c4cfcb9f
Ryan Pilgrim <ryan.pilgrim@infiniaml.com>
2019-12-09 12:29:52 -0500

    add comments for the main library module

--- autodocstring/autodocstring.py
+++ autodocstring/autodocstring.py
@@ -7,18 +7,48 @@ from parso.python.tree import ClassOrFunc, Function, Module
 
 
 def get_parse_tree(uri: str) -> Module:
+    """
+    Load the parse tree for a Python file.
+
+    Args:
+        uri (str): The path to the Python file.
+
+    Returns:
+        Module: The Python module, as parsed by Parso.
+    """
     with open(uri) as f:
         code = f.read()
 
     return parso.parse(code)
 
 
-def get_toplevel_defs(uri: str) -> Dict:
+def get_toplevel_defs(uri: str) -> List[ClassOrFunc]:
+    """
+    Load the top-level class and function definitions for the file.
+
+    Args:
+        uri (str): The path to the Python file.
+
+    Returns:
+        List[ClassOrFunc]: A list of class and function definitions for uri.
+    """
     tree = get_parse_tree(uri)
     return [*tree.iter_funcdefs(), *tree.iter_classdefs()]
 
 
 def get_enclosing_defn(uri: str, line: int, defs: List[ClassOrFunc]) -> Optional[ClassOrFunc]:
+    """
+    Recursively search for the most nested class or function definition at the specified line in the file uri.
+
+    Args:
+        uri (str): The path to the Python file.
+        line (int): The line in the file given by uri for which to find the definition.
+        defs (List[ClassOrFunc]): A list of candidate definitions for the line and file.
+
+    Returns:
+        Optional[ClassOrFunc]: The most nested function or class definition, if there is one at the given line and
+            file, else None.
+    """
     # base case
     if len(defs) == 0:
         return None
@@ -35,6 +65,30 @@ def get_enclosing_defn(uri: str, line: int, defs: List[ClassOrFunc]) -> Optional
 
 
 def get_docstring_info(uri: str, line: int) -> Optional[Dict[str, Any]]:
+    """
+    Get the docstring context information for the given file (uri) and line (line). Return the result as a dictionary
+    for JSON serialization. If no suitable definition is found, return None.
+
+    The schema of the docstring information is as follows:
+    {
+        "func_name": <str>,
+        "params": [
+            {"name": <str>, "annotation": <str>, "default": <str>},
+            ...
+        ],
+        "return_typehint": <str>,
+        "raise_types": [<str>, ...],
+        "id": <str>,
+    }
+
+    Args:
+        uri (str): The path to the Python file.
+        line (int): The line in the file given by uri for which to find the definition.
+
+    Returns:
+        Optional[Dict[str, Any]]: The docstring information dictionary for the function at the given line in the file
+            specified by uri.
+    """
     toplevel_defs = get_toplevel_defs(uri)
     enclosing_defn = get_enclosing_defn(uri, line, toplevel_defs)
     if enclosing_defn is None:
@@ -47,6 +101,15 @@ def get_docstring_info(uri: str, line: int) -> Optional[Dict[str, Any]]:
 
 
 def get_function_info(defn: Function) -> Dict[str, Any]:
+    """
+    Extract docstring information for the given function.
+
+    Args:
+        defn (Function): The function of interest.
+
+    Returns:
+        Dict[str, Any]: The docstring information for the given defun.
+    """
     parameters = [node for node in defn.children if node.type == "parameters"][0]
     params = [p for p in parameters.children if p.type == "param"]
 
@@ -73,14 +136,41 @@ def get_function_info(defn: Function) -> Dict[str, Any]:
 
 
 def to_code(node: Any) -> str:
+    """
+    Reconstruct code for the given node, and strip it of whitespace.
+
+    Args:
+        node (Any): An object representing a node of the Parso abstract syntax tree.
+
+    Returns:
+        str: The string representation of the abstract syntax tree node.
+    """
     if node is None:
         return None
     return node.get_code().strip()
 
 
-def is_arrow(node: Any) -> str:
+def is_arrow(node: Any) -> bool:
+    """
+    Predicate that tests whether the given AST node matches '->'.
+
+    Args:
+        node (Any): An object representing a node of the Parso abstract syntax tree.
+
+    Returns:
+        bool: Whether node represents the token '->'.
+    """
     return node.type == "operator" and node.value == "->"
 
 
-def is_colon(node: Any) -> str:
+def is_colon(node: Any) -> bool:
+    """
+    Predicate that tests whether the given AST node matches ':'.
+
+    Args:
+        node (Any): An object representing a node of the Parso abstract syntax tree.
+
+    Returns:
+        bool: Whether node represents the token ':'.
+    """
     return node.type == "operator" and node.value == ":"
