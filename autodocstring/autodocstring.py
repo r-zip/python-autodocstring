25494382ff2f6954d0aac7a685e5fa00b682f1fd
Ryan Pilgrim <ryan.pilgrim@infiniaml.com>
2021-01-06 16:56:07 -0500

    update to send buffers instead of file paths

--- autodocstring/autodocstring.py
+++ autodocstring/autodocstring.py
@@ -6,43 +6,55 @@ import parso
 from parso.python.tree import ClassOrFunc, Function, Module
 
 
-def get_parse_tree(uri: str) -> Module:
+def get_parse_tree_from_file(path: str) -> Module:
     """
     Load the parse tree for a Python file.
 
     Args:
-        uri (str): The path to the Python file.
+        path (str): The path to the Python file.
 
     Returns:
         Module: The Python module, as parsed by Parso.
     """
-    with open(uri) as f:
+    with open(path) as f:
         code = f.read()
 
     return parso.parse(code)
 
 
-def get_toplevel_defs(uri: str) -> List[ClassOrFunc]:
+def get_parse_tree_from_source_code(source_code: str) -> Module:
+    """
+    Load the parse tree for a buffer.
+
+    Args:
+        path (str): The Python source code.
+
+    Returns:
+        Module: The Python module, as parsed by Parso.
+    """
+    return parso.parse(source_code)
+
+
+def get_toplevel_defs(source_code: str) -> List[ClassOrFunc]:
     """
     Load the top-level class and function definitions for the file.
 
     Args:
-        uri (str): The path to the Python file.
+        path (str): The path to the Python file.
 
     Returns:
-        List[ClassOrFunc]: A list of class and function definitions for uri.
+        List[ClassOrFunc]: A list of class and function definitions for path.
     """
-    tree = get_parse_tree(uri)
+    tree = get_parse_tree_from_source_code(source_code)
     return [*tree.iter_funcdefs(), *tree.iter_classdefs()]
 
 
-def get_enclosing_defn(uri: str, line: int, defs: List[ClassOrFunc]) -> Optional[ClassOrFunc]:
+def get_enclosing_defn(line: int, defs: List[ClassOrFunc]) -> Optional[ClassOrFunc]:
     """
-    Recursively search for the most nested class or function definition at the specified line in the file uri.
+    Recursively search for the most nested class or function definition at the specified line in the file path.
 
     Args:
-        uri (str): The path to the Python file.
-        line (int): The line in the file given by uri for which to find the definition.
+        line (int): The line in the file given by path for which to find the definition.
         defs (List[ClassOrFunc]): A list of candidate definitions for the line and file.
 
     Returns:
@@ -55,18 +67,22 @@ def get_enclosing_defn(uri: str, line: int, defs: List[ClassOrFunc]) -> Optional
 
     # recursion
     for defn in defs:
-        if isinstance(defn, ClassOrFunc) and defn.start_pos[0] <= line and defn.end_pos[0] >= line:
+        if (
+            isinstance(defn, ClassOrFunc)
+            and defn.start_pos[0] <= line
+            and defn.end_pos[0] >= line
+        ):
             # search over this definition's function and class definitions
             nested_defs = [*defn.iter_funcdefs(), *defn.iter_classdefs()]
             # prefer most nested def
-            return get_enclosing_defn(uri, line, nested_defs) or defn
+            return get_enclosing_defn(line, nested_defs) or defn
 
     return None
 
 
-def get_docstring_info(uri: str, line: int) -> Optional[Dict[str, Any]]:
+def get_docstring_info(source_code: str, line: int) -> Optional[Dict[str, Any]]:
     """
-    Get the docstring context information for the given file (uri) and line (line). Return the result as a dictionary
+    Get the docstring context information for the given file (path) and line (line). Return the result as a dictionary
     for JSON serialization. If no suitable definition is found, return None.
 
     The schema of the docstring information is as follows:
@@ -82,19 +98,19 @@ def get_docstring_info(uri: str, line: int) -> Optional[Dict[str, Any]]:
     }
 
     Args:
-        uri (str): The path to the Python file.
-        line (int): The line in the file given by uri for which to find the definition.
+        source_code (str): The Python source code.
+        line (int): The line in the file given by path for which to find the definition.
 
     Returns:
         Optional[Dict[str, Any]]: The docstring information dictionary for the function at the given line in the file
-            specified by uri.
+            specified by path.
     """
-    toplevel_defs = get_toplevel_defs(uri)
-    enclosing_defn = get_enclosing_defn(uri, line, toplevel_defs)
+    toplevel_defs = get_toplevel_defs(source_code)
+    enclosing_defn = get_enclosing_defn(line, toplevel_defs)
     if enclosing_defn is None:
         return None
 
-    if type(enclosing_defn) == Function:
+    if type(enclosing_defn) is Function:
         return get_function_info(enclosing_defn)
 
     return None
@@ -115,19 +131,28 @@ def get_function_info(defn: Function) -> Dict[str, Any]:
 
     return_typehint = None
     # if there's a return typehint, get it
-    if any(is_arrow(n) for n in defn.children) and any(is_colon(n) for n in defn.children):
+    if any(is_arrow(n) for n in defn.children) and any(
+        is_colon(n) for n in defn.children
+    ):
         # take only the stuff between the arrow and the colon
         children = dropwhile(lambda x: not is_arrow(x), defn.children)
         children = list(takewhile(lambda x: not is_colon(x), children))
         if len(children) == 2:
             return_typehint = to_code(list(children)[1])
 
-    raise_types = [to_code(stmt.children[1].children[0]) for stmt in defn.iter_raise_stmts()]
+    raise_types = [
+        to_code(stmt.children[1].children[0]) for stmt in defn.iter_raise_stmts()
+    ]
 
     return {
         "func_name": defn.name.value,
         "params": [
-            {"name": p.name.value, "annotation": to_code(p.annotation), "default": to_code(p.default)} for p in params
+            {
+                "name": p.name.value,
+                "annotation": to_code(p.annotation),
+                "default": to_code(p.default),
+            }
+            for p in params
         ],
         "return_typehint": return_typehint,
         "raise_types": raise_types,
@@ -135,7 +160,7 @@ def get_function_info(defn: Function) -> Dict[str, Any]:
     }
 
 
-def to_code(node: Any) -> str:
+def to_code(node: Any) -> Optional[str]:
     """
     Reconstruct code for the given node, and strip it of whitespace.
 
--- examples/client.py
+++ examples/client.py
@@ -3,6 +3,8 @@ from jsonrpcclient.clients.http_client import HTTPClient
 from autodocstring.constants import DEFAULT_HOST, DEFAULT_PORT
 
 if __name__ == "__main__":
+    with open("autodocstring/autodocstring.py") as f:
+        code = f.read()
     client = HTTPClient(f"http://{DEFAULT_HOST}:{DEFAULT_PORT}")
-    result = client.request("get_docstring_info", uri="autodocstring/autodocstring.py", line=29).data.result
+    result = client.request("get_docstring_info", source_code=code, line=29).data.result
     print(f"Received result: {result}")
